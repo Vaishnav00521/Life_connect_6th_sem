bc5b9821b0fb94e2aa00dd37ed613167e2907c5b
Vaishnav00521 <vaishnavshah.005@gmail.com>
2026-04-07 01:02:50 +0530

    fix(build): revert UserDetailsServiceAutoConfiguration exclusion to fix render compilation error

--- src/main/java/com/lifeconnect/LifeConnect/LifeConnectApplication.java
+++ src/main/java/com/lifeconnect/LifeConnect/LifeConnectApplication.java
@@ -3,9 +3,7 @@ package com.lifeconnect.LifeConnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class LifeConnectApplication {
 
 	public static void main(String[] args) {
