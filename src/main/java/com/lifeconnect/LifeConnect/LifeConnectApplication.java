a81e88f2995842341b21e54b292f537f86cc1209
Vaishnav00521 <vaishnavshah.005@gmail.com>
2026-04-07 00:55:09 +0530

    chore(config): disable OSIV and default security user logs for clean production startup

--- src/main/java/com/lifeconnect/LifeConnect/LifeConnectApplication.java
+++ src/main/java/com/lifeconnect/LifeConnect/LifeConnectApplication.java
@@ -3,7 +3,9 @@ package com.lifeconnect.LifeConnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class LifeConnectApplication {
 
 	public static void main(String[] args) {
