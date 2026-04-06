7f3ead130112c44693559db0772ea9d2ac5efb38
Vaishnav00521 <vaishnavshah.005@gmail.com>
2026-04-07 01:14:36 +0530

    chore(security): use excludeName to silence UserDetailsService AutoConfig safely

--- src/main/java/com/lifeconnect/LifeConnect/LifeConnectApplication.java
+++ src/main/java/com/lifeconnect/LifeConnect/LifeConnectApplication.java
@@ -3,7 +3,7 @@ package com.lifeconnect.LifeConnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration"})
 public class LifeConnectApplication {
 
 	public static void main(String[] args) {
